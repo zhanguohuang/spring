8998afaf31271d1dd70e17236e2a53db2ff9780e
zhanguohuang <zhanguohuang@szhuarong.com>
2017-05-24 15:19:50 +0800

    给一个测试类，用于加载spring基本组件的所有配置文件，目的是为了汇总知识

---
+++ src/test/java/test/CollectionTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CollectionTest {
+
+	private static final String[] classPath = {"aop/aop.xml", "autowired/autowired.xml", "aware/aware.xml", 
+			"component/component.xml", "cycledepend/cycledepend.xml", "factory/beanFactory.xml", 
+			"listener/listener.xml", "lookup/lookupTest.xml", "postprocess/postprocess.xml"};
+	
+	public static void main(String[] args) {
+		ApplicationContext app = new ClassPathXmlApplicationContext(classPath);
+		String[] beanNames = app.getBeanDefinitionNames();
+		System.out.println("-=-=-=-=-show all beanName-=-=-=-=-");
+		for(String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+}
